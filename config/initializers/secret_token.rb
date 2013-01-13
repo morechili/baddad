7395b97f2d58f15ee747a9a7ec03589e9a07dc4d
Paul <github@pauldinin.com>
2013-01-13 07:45:09 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baddad::Application.config.secret_token = '0b1be195270535b2f7e6bacd173c9d7f3e01cebbf2d42c9d4c65b786f2bbdd3e6fff9b1f9aec7ca8bd082dbf0c37da191a00722214e0f1cf70e504a13a11cecf'
